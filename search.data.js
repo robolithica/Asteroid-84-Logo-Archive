c18e441035b21f2206d6d3cb1fc7424cd9335141
Asteroid-84 <robolithic@gmail.com>
2026-06-11 15:40:27 -0700

    Create search.data.js

---
+++ search.data.js
@@ -0,0 +1,10 @@
+const pages = [
+    { title: "He-Man and the Masters of the Universe", url: "heman.html"
+    keywords: "Heman motu masters eternia"
+},
+    { title: "G.I. Joe", url: "gijoe.html" },
+    { title: "ThunderCats", url: "thundercats.html" },
+    { title: "Voltron", url: "voltron.html" },
+    { title: "SilverHawks", url: "silverhawks.html" },
+    { title: "M.A.S.K.", url: "mask.html" }
+];
