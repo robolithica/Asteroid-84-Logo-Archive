8aa8ce584baa04df7ca81540f5e54b83bb797235
Asteroid-84 <robolithic@gmail.com>
2026-06-12 08:20:51 -0700

    Update search-data.js

--- search-data.js
+++ search-data.js
@@ -1,9 +1,11 @@
 const pages = [
     { title: "He-Man and the Masters of the Universe", url: "heman.html",
-    keywords: "Heman motu masters eternia"
+    keywords: "Heman motu masters eternia 1983 1984 1985 skeletor filmation marvel mattel"
 },
-    { title: "G.I. Joe", url: "PAGES/GIJoe.html", keywords: "GIJoe yo joe duke scarlett snake cobra" },
-    { title: "ThunderCats", url: "thundercats.html" },
+    { title: "G.I. Joe", url: "PAGES/GIJoe.html", keywords: "GIJoe yo joe duke scarlett snake cobra hasbro 1982 1983 1984 1985 1986 1987 1988
+        1989 Sunbow Marvel " },
+    { title: "ThunderCats", url: "ThunderCats.html", keywords: "thunder cats thundercats mumm-ra lion-o tigra cheetara thundera panthro 1985
+        1986 1987 1988 1989 rankin bass ljn star},
     { title: "Voltron", url: "voltron.html" },
     { title: "SilverHawks", url: "silverhawks.html" },
     { title: "M.A.S.K.", url: "mask.html" }
