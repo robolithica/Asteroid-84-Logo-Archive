fd765e715f3ff9b55de851d14204c33edecb2c29
Asteroid-84 <robolithic@gmail.com>
2026-06-12 11:15:11 -0700

    Update search-data.js

--- search-data.js
+++ search-data.js
@@ -5,7 +5,7 @@ const pages = [
     { title: "G.I. Joe", url: "PAGES/GIJoe.html", keywords: "GIJoe yo joe duke scarlett snake cobra hasbro 1982 1983 1984 1985 1986 1987 1988 1989 Sunbow Marvel" },
     { title: "ThunderCats", url: "ThunderCats.html", keywords: "thunder cats thundercats mumm-ra lion-o tigra cheetara thundera panthro 1985 1986 1987 1988 1989 rankin bass ljn star"},
     { title: "Voltron", url: "voltron.html" },
-    { title: "SilverHawks", url: "silverhawks.html" },
+    { title: "SilverHawks", url: "SilverHawks.html" },
     { title: "M.A.S.K.", url: "mask.html" }
 ];
-alert("search-data loaded");
+
