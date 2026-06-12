fdc0810e73864ddfd3dbcc8e22b359291824eda9
Asteroid-84 <robolithic@gmail.com>
2026-06-11 18:47:23 -0700

    Update search-data.js

--- search-data.js
+++ search-data.js
@@ -2,7 +2,7 @@ const pages = [
     { title: "He-Man and the Masters of the Universe", url: "heman.html",
     keywords: "Heman motu masters eternia"
 },
-    { title: "G.I. Joe", url: ".../PAGES/GIJoe.html", keywords: "GIJoe yo joe duke scarlett snake cobra" },
+    { title: "G.I. Joe", url: "..PAGES/GIJoe.html", keywords: "GIJoe yo joe duke scarlett snake cobra" },
     { title: "ThunderCats", url: "thundercats.html" },
     { title: "Voltron", url: "voltron.html" },
     { title: "SilverHawks", url: "silverhawks.html" },
